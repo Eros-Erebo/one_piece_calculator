53f03d75e00e96874f73f4e97b589e0e096004ae
Eros-Erebo <147782490+Eros-Erebo@users.noreply.github.com>
2024-05-13 18:26:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -106,8 +106,10 @@ function calcularUpEPontos(nivelAtual, xpTotal, local) {
 
     // Se o XP total não for suficiente para o próximo nível, retorna o nível atual
     if (nivel_alcancado === nivelAtual) {
-        var xp_sobrando = xp_atual - xpTotal;
-        return [nivelAtual, pontos_recebidos, xp_sobrando, vezes_upadas];
+        var xp_sobrando = xpTotal;
+        var vezes_upadas = 0;
+        var xp_atual = [xpTotal / niveis_xp[nivelAtual]];
+        return [nivelAtual, pontos_recebidos, xp_sobrando, vezes_upadas, xp_atual];
     }
 
     return [nivel_alcancado, pontos_recebidos, xp_faltante_para_proximo_nivel, xpTotal, vezes_upadas];
